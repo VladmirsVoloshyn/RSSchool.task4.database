f96b407d0bd3ff5da89df197b904f7262c280977
VladmirsVoloshyn <vovapvpgg1994@gmail.com>
2021-08-23 15:37:07 +0300

    initial

--- app/src/main/java/com/example/petdatabase/ui/petlist/PetListAdapter.kt
+++ app/src/main/java/com/example/petdatabase/ui/petlist/PetListAdapter.kt
@@ -6,13 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Space
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.petdatabase.R
 import com.example.petdatabase.model.Pet
-import kotlin.math.PI
 
 class PetListAdapter(
     private val listPets: ArrayList<Pet>,
